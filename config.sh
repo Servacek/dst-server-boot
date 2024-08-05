f5dfa297cc42c2399ddedf34ca4cf42252469a96
Servacek <filip.mitka007@gmail.com>
2024-08-05 13:10:05 +0200

    initial commit

---
+++ config.sh
@@ -0,0 +1,98 @@
+################## CONFIGURATION #################
+
+VALIDATE=true;
+GAMEID=343050;
+STEAMCMD="/usr/games/steamcmd";
+LOGIN="anonymous";
+GAMEDIR="/home/steam/.klei/DoNotStarveTogether/CZSK/Game";
+MODS_SETUP_FILE_PATH="$GAMEDIR/mods/dedicated_server_mods_setup.lua";
+MODS_SETUP_FILE_BACKUP_PATH=$MODS_SETUP_FILE_PATH.bak;
+X64=true;
+DST_BIN="${GAMEDIR}/$(if [[ $X64 == true ]]; then echo "bin64"; else echo "bin"; fi)/dontstarve_dedicated_server_nullrenderer$(if [[ $X64 == true ]]; then echo "_x64"; fi)"
+
+################## COMMAND LINE OPTIONS #################
+
+# https://support.klei.com/hc/en-us/articles/360029556192-Dedicated-Server-Command-Line-Options-Guide
+
+# Set the name of the cluster directory that this server will use.
+# The server will expect to find the cluster.ini file in the following location:
+# <persistent_storage_root>/<conf_dir>/<cluster>/cluster.ini,
+# where <persistent_storage_root> and <conf_dir> are the values
+# set by the -persistent_storage_root and -conf_dir options. The default is "Cluster_1".
+CLUSTER="CZSK"
+
+# Change the name of your configuration directory.
+# This name should not contain any slashes.
+# The full path to your files will be <persistent_storage_root>/<conf_dir>
+# where <persistent_storage_root> is the value set by the -persistent_storage_root option.
+# The default is: "DoNotStarveTogether".
+CONF_DIR="DoNotStarveTogether"
+
+# Change the directory that your configuration directory resides in.
+# This must be an absolute path.
+# The full path to your files will be <persistent_storage_root>/<conf_dir>/
+# where <conf_dir> is the value set by -conf_dir.
+# The default for this option depends on the platform:
+#   Windows: <Your documents folder>/Klei
+#   Mac OSX: <Your home folder>/Documents/Klei
+#   Linux: ~/.klei
+PERSISTENT_STORAGE_ROOT="home/steam/.klei"
+
+# Updates server mods listed in the dedicated_server_mods_setup.lua file but does not launch a server.
+ONLY_UPDATE_SERVER_MODS=false
+
+# Skips mod updates.
+SKIP_UPDATE_SERVER_MODS=false
+
+# Create a backup of the previous log files each time the server is run.
+# The backups will be stored in a directory called "backup" in the same directory as server.ini.
+BACKUP_LOG_COUNT=8
+
+# Valid values: 15 .. 60
+# This is the number of times per-second that the server sends updates to clients.
+# Increasing this may improve precision, but will result in more network traffic.
+# This option overrides the [NETWORK] / tick_rate setting in cluster.ini.
+# It is recommended to leave this at the default value of 15.
+# If you do change this option, it is recommended that you do so only for LAN games,
+# and use a number evenly divisible into 60 (15, 20, 30).
+TICK=15
+
+# Valid values: 1..64
+# Set the maximum number of players that will be allowed to join the game.
+# This option overrides the [GAMEPLAY] / max_players setting in cluster.ini.
+PLAYERS=6
+
+# Change the address that the server binds to when listening for player connections.
+# This is an advanced feature that most people will not need to use.
+BIND_IP="127.0.0.1"
+
+# Whether to interpret input in stdin as console commands.
+CONSOLE=true
+
+################## SHARDS #################
+
+# Folder names for the individual shards this server has.
+# At least one shard is required.
+SHARDS=("Master" "Caves")
+# Assign CPU cores to shards. The first number in this array is assigned
+# to the first shard in the SHARDS array and so on.
+# Only one CPU core is needed per shard since DST doesn't know how to work with multiple cores anyway.
+# If you keep this empty, all available shards will be set as assigned so it will
+# leave this task on your task scheduler to choose the best core for this process.
+CPUCORES=(14 15)      # Range 14 - 15
+# Explicitly assign ports to shards, this is the same as the "server_port" config in each
+# of the shard's server.ini files.
+# Ports are assigned in the order of the SHARDS array.
+# If you keep this empty, the configuration from the server.ini files will be used instead.
+PORTS=(11013 11014)   # Range 11012 - 11015
+
+# The CPU core the Master shard should run on, i.e. the first one from the CPUCORES array.
+# It is assumed this core is not used by another process.
+# This is used by the server updating process as well before the server itself starts.
+MAIN_CPUCORE=${CPUCORES[0]}
+
+################## SESSIONS #################
+
+SCREEN_SESSIONS=({$SHARDS[@]} + "Update")
+
+#############################################
---
+++ run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+DEFAULT_CONFIG_FILE="default_config.sh"
+CONFIG_FILE="config.sh"
+UPDATE_LOG_FILE="update.log"
+
+echo "Loading configuration..."
+if [ -f DEFAULT_CONFIG_FILE ]; then
+    if [ ! -f CONFIG_FILE ]; then
+        cp DEFAULT_CONFIG_FILE CONFIG_FILE
+    fi
+else
+    echo "Configuration failed to load: ${DEFAULT_CONFIG_FILE} not found. Try to redownload the script."
+    exit 1
+fi
+
+source default_config.sh # For compatibility with newer configurations not yet configured locally.
+source config.sh
+
+###########################################
+
+# Make sure no other session created by this script are currently running
+for SESSIONS in ${SCREEN_SESSIONS[@]}; do
+    SESSION="${CLUSTER}_${SESSIONS}"
+    if screen -ls | grep -q "${SESSION}"; then
+        echo "Cannot start the server screen sessions because the session \"${SESSION}\" already exists."
+    fi
+done
+
+if [[ $VALIDATE == true ]]; then
+    # Backup the mods setup file since it's being overridden by validation.
+    echo "Backing up the mods setup file..."
+    mv "$MODS_SETUP_FILE_PATH" "$MODS_SETUP_FILE_BACKUP_PATH"
+fi
+
+# Start up the updating process but do not fork, we want to wait until it finishes.
+echo "Staring the server update process..."
+taskset -c ${MAIN_CPUCORE} \
+    screen -m -U -D -S ${CLUSTER}_Update \
+        bash -c "${STEAMCMD} \
+            +force_install_dir ${GAMEDIR} \
+            +login ${LOGIN} \
+            +app_update ${GAMEID} \
+            $(if [[ $VALIDATE == true ]]; then "validate"; else ""; fi) \
+            +quit 2>&1 \
+        | tee ${UPDATE_LOG_FILE} | tee /dev/tty" # Log output to console as well.
+
+if [[ exists "$MODS_SETUP_FILE_BACKUP_PATH" ]]; then
+    echo "Restoring the mods setup file..."
+    mv "$MODS_SETUP_FILE_BACKUP_PATH" "$MODS_SETUP_FILE_PATH"
+fi
+
+if [[ $? -ne 0 ]]; then
+    echo "Updating server proccess failed! Status: $?"
+    exit 1
+else
+    echo "Updating server proccess finished successfully."
+fi
+
+echo "Starting up the shards..."
+for INDEX in ${!SHARDS[@]}; do
+    SHARD_NAME=${SHARDS[$INDEX]} # Should always have an value, since names are required.
+
+    # Optional
+    CPU_CORE=${CPUCORES[$INDEX]}
+    PORT=${PORTS[$INDEX]}
+
+    echo "Starting ${SHARD_NAME}..."
+    taskset -c $(if [[ -n $CPU_CORE ]]; then echo "${CPU_CORE}"; else echo "0-$(($(nproc)-1))"; fi) \
+        screen -m -U -t "${SHARD_NAME}" -S "${CLUSTER}_${SHARD_NAME}" "$DST_BIN" \
+            -persistent_storage_root "$PERSISTENT_STORAGE_ROOT" \
+            -conf_dir "$CONF_DIR" \
+            -cluster "$CLUSTER" \
+            -shard "$SHARD_NAME" \
+            -backup_log_count "$BACKUP_LOG_COUNT" \
+            -only_update_server_mods "$ONLY_UPDATE_SERVER_MODS" \
+            -skip_update_server_mods "$SKIP_UPDATE_SERVER_MODS" \
+            -bind_ip "$BIND_IP" \
+            -tick "$TICK" \
+            $(if [[ $PORT != "" ]]; then "-port $PORT"; else ""; fi) \
+            $(if [[ $CONSOLE == true ]]; then "-console"; else ""; fi)
+
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to start ${SHARD_NAME}! Status: $?"
+    else
+        echo "Started ${SHARD_NAME}!"
+    fi
+done
