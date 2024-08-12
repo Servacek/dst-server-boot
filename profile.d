b2c0fae02128084fb2bd5c68aa7b1d12e47a3ff7
Servacek <filip.mitka007@gmail.com>
2024-08-12 16:39:48 +0200

    Fixed profile.d being a bit broken after the last update.

--- profile.d
+++ profile.d
@@ -24,26 +24,26 @@ if groups $USER | grep -q "\b${GROUP_NAME}\b"; then
     echo ""
     echo "AVAILABLE ALIASES"
 
-    add_alias "wmods" "cd ${GAMEDIR}/ugc_mods/${CLUSTER_NAME}/Master/content/${G                                                                                                             AMEID}" "Change your pwd to the ${CLUSTER} server's ugc_mods directory storing n                                                                                                             ewer workshop mods."
-    add_alias "mods" "cd ${GAMEDIR}/mods" "Change your pwd to the ${CLUSTER} ser                                                                                                             ver's mods directory."
-    add_alias "sboot" "cd ${GAMEDIR}/boot" "Change your pwd to the ${CLUSTER} se                                                                                                             rver's boot directory."
+    add_alias "wmods" "cd ${GAMEDIR}/ugc_mods/${CLUSTER_NAME}/Master/content/${GAMEID}" "Change your pwd to the ${CLUSTER} server's ugc_mods directory storing newer workshop mods."
+    add_alias "mods" "cd ${GAMEDIR}/mods" "Change your pwd to the ${CLUSTER} server's mods directory."
+    add_alias "sboot" "cd ${GAMEDIR}/boot" "Change your pwd to the ${CLUSTER} server's boot directory."
 
     echo ""
 
     for SHARD in ${SHARDS[@]}; do
-        add_alias "${SHARD,,}" "cd ${CLUSTER_PATH}/${CLUSTER}/${SHARD}" "Change                                                                                                              your pwd to the ${CLUSTER} server's ${SHARD} shard directory."
-        add_alias "c_${SHARD,,}" "screen -r ${SESSION_OWNER}/${SHARD_SESSION_PRE                                                                                                             FIX}${SHARD}" "Open the console for the ${SHARD} shard."
+        add_alias "${SHARD,,}" "cd ${CLUSTER_PATH}/${CLUSTER}/${SHARD}" "Change your pwd to the ${CLUSTER} server's ${SHARD} shard directory."
+        add_alias "c_${SHARD,,}" "screen -r ${SESSION_OWNER}/${SHARD_SESSION_PREFIX}${SHARD}" "Open the console for the ${SHARD} shard."
     done
 
     # TODO: Make this display only the server's processes.
-    alias pcores='for pid in $(ps -a -o pid=); do source_file=$(ps -p $pid -o ar                                                                                                             gs=); affinity=$(taskset -pc $pid 2>/dev/null); \
-                  echo "PID: $pid, Source File: $source_file, Affinity: $affinit                                                                                                             y"; done'
+    # alias pcores='for pid in $(ps -a -o pid=); do source_file=$(ps -p $pid -o args=); affinity=$(taskset -pc $pid 2>/dev/null); \
+    #               echo "PID: $pid, Source File: $source_file, Affinity: $affinity"; done'
 
     echo ""
-    add_alias "c_reload" "systemctl reload ${SERVICE}.service" "The same effect                                                                                                              as c_reload() in game but is being executed by systemctl itself."
-    add_alias "c_reboot" "systemctl restart ${SERVICE}.service" "The same effect                                                                                                              as c_reboot() in game but is being executed by systemctl itself."
-    add_alias "c_start" "systemctl start ${SERVICE}.service" "Updates the game a                                                                                                             nd starts the shards. The same as running "systemctl start" on the server's serv                                                                                                             ice."
-    add_alias "c_shutdown" "systemctl stop ${SERVICE}.service" "The same effect                                                                                                              as c_shutdown() in game but is being executed by systemctl itself."
+    add_alias "c_reload" "systemctl reload ${SERVICE}.service" "The same effect as c_reload() in game but is being executed by systemctl itself."
+    add_alias "c_reboot" "systemctl restart ${SERVICE}.service" "The same effect as c_reboot() in game but is being executed by systemctl itself."
+    add_alias "c_start" "systemctl start ${SERVICE}.service" "Updates the game and starts the shards. The same as running "systemctl start" on the server's service."
+    add_alias "c_shutdown" "systemctl stop ${SERVICE}.service" "The same effect as c_shutdown() in game but is being executed by systemctl itself."
     echo ""
     #cat "${BOOT_DIR}/commands.motd" # Display our custom MOTD
     echo "###############################################################"
