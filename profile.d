139546ac0bdb921b1478cd72f0bfd1350677b6bb
Servacek <filip.mitka007@gmail.com>
2024-08-17 13:57:12 +0200

    Fixed some issues in profile.d

--- profile.d
+++ profile.d
@@ -10,10 +10,11 @@ exit() {
    echo "Profile.d script exited prematurely."
 }
 
-echo "######################### ${CLUSTER} SERVER #########################"
 BOOT_DIRECTORY="/home/steam/.klei/DoNotStarveTogether/.game/boot"
 cd $BOOT_DIRECTORY
 source "${BOOT_DIRECTORY}/constants.sh"
+
+echo "######################### ${CLUSTER} SERVER #########################"
 echo ""
 echo "AVAILABLE ALIASES"
 
@@ -33,7 +34,6 @@ add_alias "ports" ' \
 for session in ${SHARD_SCREEN_SESSIONS[@]}; do \
     screen_pid=$(sudo -u "${SESSION_OWNER}" screen -ls | grep "${session}" | awk "{print $1}" | cut -d. -f1 | sed "s/^[ \t]*//;s/[ \t]*$//"); \
     pid=$(_deepest="$screen_pid"; while [[ (! -z "$_deepest") && $(pgrep -P "$_deepest") ]]; do _deepest=$(pgrep -P "$_deepest"); done; echo "$_deepest"); \
-    pname=$(if [[ ! -z "$pid" ]]; then echo $(ps -p "$pid" -o comm=); fi); \
     if [[ -z "$pid" ]]; then \
         echo "${session}:"; \
         netstat -tulnp | grep <PID>; \
