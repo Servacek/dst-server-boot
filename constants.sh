bf94fd42960067f1b5e1c3a08872d4f5cb390e2f
Servacek <filip.mitka007@gmail.com>
2024-08-18 17:04:23 +0200

    Fixed the SCRIPT_DIRECTORY constant.

--- constants.sh
+++ constants.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-SCRIPT_DIRECTORY=$(dirname -- "${0}")
+SCRIPT_DIRECTORY=$(dirname $(realpath "${0}"))
 PWD=$(pwd)
 
 DEFAULT_CONFIG_FILE="config/default_config.sh"
